81ca2801ace47396b672864c2cd32eae2d34bb49
danielvilla18 <danielvilla18@1a5bfdbd-ab35-397e-2a32-2454a375b999>
2010-11-09 16:45:40 +0000

    Agregado la tabla que tiene los campos del objeto a setear

--- trunk/JTestDesigner/src/com/teg/vista/NewDialog.java
+++ trunk/JTestDesigner/src/com/teg/vista/NewDialog.java
@@ -11,18 +11,43 @@
 
 package com.teg.vista;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Daniel
  */
 public class NewDialog extends javax.swing.JDialog {
 
+    private ArrayList<Field> camposClase;
+     
+     private DefaultTableModel tableModel;
+    
     /** Creates new form NewDialog */
     public NewDialog(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
+        LlenarTabla();
+        
     }
 
+    private void LlenarTabla()
+    {
+
+        tableModel = new DefaultTableModel(new Object[]{"Campo", "Valor"}, camposClase.size());
+        tablaCampos.setModel(tableModel);
+
+        for (int i = 0; i < camposClase.size(); i++) {
+
+            tableModel.setValueAt(camposClase.get(i).getName(), i, 0);
+        }
+        tablaCampos.repaint();
+
+        
+
+    }
     /** This method is called from within the constructor to
      * initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is
@@ -33,20 +58,38 @@ public class NewDialog extends javax.swing.JDialog {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tablaCampos = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
+        tablaCampos.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+
+            }
+        ));
+        jScrollPane1.setViewportView(tablaCampos);
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 380, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(58, 58, 58)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 293, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(64, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 231, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(32, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 209, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(74, 74, 74))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -63,7 +106,7 @@ public class NewDialog extends javax.swing.JDialog {
             .addGroup(layout.createSequentialGroup()
                 .addGap(33, 33, 33)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(36, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -72,22 +115,25 @@ public class NewDialog extends javax.swing.JDialog {
     /**
     * @param args the command line arguments
     */
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                NewDialog dialog = new NewDialog(new javax.swing.JFrame(), true);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
-            }
-        });
-    }
-
+  
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tablaCampos;
     // End of variables declaration//GEN-END:variables
+
+    /**
+     * @return the camposClase
+     */
+    public ArrayList<Field> getCamposClase() {
+        return camposClase;
+    }
+
+    /**
+     * @param camposClase the camposClase to set
+     */
+    public void setCamposClase(ArrayList<Field> camposClase) {
+        this.camposClase = camposClase;
+    }
 
 }
