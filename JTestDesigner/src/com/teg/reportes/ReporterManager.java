a9af571726fd19582d7f2ab4678d00381204df2c
mariale.uribe <mariale.uribe@1a5bfdbd-ab35-397e-2a32-2454a375b999>
2011-01-18 20:03:32 +0000

    Manejo de reportes en PDF.

--- JTestDesigner/src/com/teg/reportes/ReporterManager.java
+++ JTestDesigner/src/com/teg/reportes/ReporterManager.java
@@ -2,20 +2,24 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.teg.reportes;
 
-import java.io.FileOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.FileOutputStream;
 
-import com.lowagie.text.*;
-import com.lowagie.text.html.simpleparser.HTMLWorker;
-import com.lowagie.text.html.simpleparser.StyleSheet;
-import com.lowagie.text.pdf.PdfWriter;
-import java.io.FileReader;
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.apache.commons.io.FileUtils;
+
+import org.allcolor.yahp.converter.CYaHPConverter;
+import org.allcolor.yahp.converter.IHtmlToPdfTransformer;
+import org.allcolor.yahp.converter.IHtmlToPdfTransformer.CConvertException;
 
 /**
  *
@@ -23,37 +27,60 @@ import java.util.logging.Logger;
  */
 public class ReporterManager {
 
+    private static CYaHPConverter converter = new CYaHPConverter();
+
     public ReporterManager() {
     }
 
-    public void HtmlToPDF(ArrayList<String> rutasHTML, String rutaPDF) {
+    public void htmlToPdfConverter(ArrayList<String> rutasHTML, String pdfOutfile, String folderLocation) {
+
+        FileOutputStream out = null;
+        String contenido = "";
+        for (String file : rutasHTML) {
+            try {
+                File miHtml = new File(file);
+                contenido = contenido + "<br><br>"
+                        + FileUtils.readFileToString(miHtml);
+            } catch (IOException ex) {
+                Logger.getLogger(ReporterManager.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
 
         try {
-            Document document = new Document();
-            StyleSheet style = new StyleSheet();
-            style.loadTagStyle("body", "leading", "16,0");
+            String fontPath = null;
+            File fout = new File(pdfOutfile + System.getProperty("file.separator"));
+            out = new FileOutputStream(fout);
+            Map properties = new HashMap();
+            List headerFooterList = new ArrayList();
 
-            PdfWriter.getInstance(document, new FileOutputStream("/Users/maya/HelloWorld.pdf"));
+            headerFooterList.add(new IHtmlToPdfTransformer.CHeaderFooter("<table width=\"100%\"><tbody><tr><td align=\"left\">" + "Generated with YaHPConverter.</td><td align=\"right\">Page <pagenumber>/<" + "pagecount></td></tr></tbody></table>", IHtmlToPdfTransformer.CHeaderFooter.HEADER));
+            headerFooterList.add(new IHtmlToPdfTransformer.CHeaderFooter("<table width=\"100%\"><tbody><tr><td align=\"left\">" + "Generated with YaHPConverter.</td><td align=\"right\">Page <pagenumber>/<" + "pagecount></td></tr></tbody></table>", IHtmlToPdfTransformer.CHeaderFooter.FOOTER));
 
-            document.open();
-            document.add(new Paragraph(" "));
+            properties.put(IHtmlToPdfTransformer.PDF_RENDERER_CLASS,
+                    IHtmlToPdfTransformer.FLYINGSAUCER_PDF_RENDERER);
 
-            for (String ruta : rutasHTML) {
-                ArrayList p = HTMLWorker.parseToList(new FileReader(ruta), style);
-                for (int k = 0; k < p.size(); ++k) {
-                    document.add((Element) p.get(k));
-                    document.add(new Paragraph(" "));
-                }
-
-                document.add(new Paragraph(" "));
+            if (fontPath != null) {
+                properties.put(IHtmlToPdfTransformer.FOP_TTF_FONT_PATH, fontPath);
             }
 
-            document.close();
+            converter.convertToPdf(contenido,
+                    IHtmlToPdfTransformer.A4P, headerFooterList,
+                    "file://" + folderLocation + "/", out, properties);
+            System.out.println("mihtml \n" + contenido);
+
+            out.flush();
+            out.close();
         } catch (IOException ex) {
             Logger.getLogger(ReporterManager.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (DocumentException ex) {
+        } catch (CConvertException ex) {
             Logger.getLogger(ReporterManager.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                out.flush();
+                out.close();
+            } catch (IOException ex) {
+                Logger.getLogger(ReporterManager.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
-
 }
